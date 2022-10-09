7feaea701568d1b03f7800a6237ce312db491f15
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2022-10-09 12:00:28 -0700

    New goal added for ScarecrowEntity. Very basic implementation so far. Currently, the crop block below the ScareCrowEntity will age quickly. In the goal register the probability of having the crop age can be altered.

--- src/main/java/team/bananabank/hauntedfields/entity/ScarecrowEntity.java
+++ src/main/java/team/bananabank/hauntedfields/entity/ScarecrowEntity.java
@@ -1,13 +1,21 @@
 package team.bananabank.hauntedfields.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.entity.EntityAccess;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -15,6 +23,8 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import java.util.ArrayList;
 
 public class ScarecrowEntity extends Monster implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
@@ -40,6 +50,7 @@ public class ScarecrowEntity extends Monster implements IAnimatable {
         //this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
         //this.goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1.0D, true, 4, () -> true));
         this.goalSelector.addGoal(7, new ScarecrowEntity.MoveRandomlyGoal(this, 1.0D));
+        this.goalSelector.addGoal(7, new BenefitCropsGoal(this, 0.1D));
         //this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglin.class)); // Add nearby crows if added to mod
         //this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
         //this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
@@ -81,6 +92,82 @@ public class ScarecrowEntity extends Monster implements IAnimatable {
         @Override
         public boolean canUse() {
             return super.canUse() && !isNightTime(mob.level);
+        }
+    }
+
+    private class BenefitCropsGoal extends Goal {
+        protected final PathfinderMob mob;
+        protected final double probability;
+
+        public BenefitCropsGoal(PathfinderMob mob, double probability) {
+            this.mob = mob;
+            // How often will scarecrow as % decimal
+            this.probability = probability;
+        }
+        @Override
+        public boolean canUse() {
+            return !isNightTime(mob.level);
+        }
+
+
+        @Override
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
+
+        @Override
+        public void tick() {
+            // Mostly from bee class BeeGrowCropGoal class.
+
+            if (ScarecrowEntity.this.random.nextDouble() <= this.probability) {
+                BlockPos blockpos = ScarecrowEntity.this.blockPosition();
+                BlockState blockstate = ScarecrowEntity.this.level.getBlockState(blockpos);
+                Block block = blockstate.getBlock();
+
+                // Deal with bug where block at entity position was counting the farmland below
+                BlockPos blockposAbove = ScarecrowEntity.this.blockPosition().above();
+                BlockState blockstateAbove = ScarecrowEntity.this.level.getBlockState(blockposAbove);
+                Block blockAbove = blockstateAbove.getBlock();
+
+                if (block instanceof FarmBlock) {
+                    blockpos = blockposAbove;
+                    blockstate = blockstateAbove;
+                    block = blockAbove;
+                }
+
+                boolean flag = false;
+                IntegerProperty integerproperty = null;
+
+
+                if (blockstate.is(BlockTags.BEE_GROWABLES)) {
+                    if (block instanceof CropBlock) {
+                        CropBlock cropblock = (CropBlock) block;
+                        if (!cropblock.isMaxAge(blockstate)) {
+                            flag = true;
+                            integerproperty = cropblock.getAgeProperty();
+                        }
+                    } else if (block instanceof StemBlock) {
+                        int j = blockstate.getValue(StemBlock.AGE);
+                        if (j < 7) {
+                            flag = true;
+                            integerproperty = StemBlock.AGE;
+                        }
+                    } else if (blockstate.is(Blocks.SWEET_BERRY_BUSH)) {
+                        int k = blockstate.getValue(SweetBerryBushBlock.AGE);
+                        if (k < 3) {
+                            flag = true;
+                            integerproperty = SweetBerryBushBlock.AGE;
+                        }
+                    } else if (blockstate.is(Blocks.CAVE_VINES) || blockstate.is(Blocks.CAVE_VINES_PLANT)) {
+                        ((BonemealableBlock) blockstate.getBlock()).performBonemeal((ServerLevel) ScarecrowEntity.this.level, ScarecrowEntity.this.random, blockpos, blockstate);
+                    }
+
+                    if (flag) {
+                        ScarecrowEntity.this.level.levelEvent(2005, blockpos, 0);
+                        ScarecrowEntity.this.level.setBlockAndUpdate(blockpos, blockstate.setValue(integerproperty, Integer.valueOf(blockstate.getValue(integerproperty) + 1)));
+                    }
+                }
+            }
         }
     }
 }
