72ea8c172b26cb297f4718f93b8e2cc1e1e7a4db
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2022-10-08 21:53:24 -0700

    Make the Scarecrow only walk around during the night

--- src/main/java/team/bananabank/hauntedfields/entity/ScarecrowEntity.java
+++ src/main/java/team/bananabank/hauntedfields/entity/ScarecrowEntity.java
@@ -1,16 +1,11 @@
 package team.bananabank.hauntedfields.entity;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.animal.IronGolem;
-import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.ZombifiedPiglin;
-import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -38,24 +33,22 @@ public class ScarecrowEntity extends Monster implements IAnimatable {
     protected void registerGoals() {
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
-        this.addBehaviourGoals();
+        this.addBehaviorGoals();
     }
 
-    protected void addBehaviourGoals() {
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
-        this.goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1.0D, true, 4, () -> true));
-        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglin.class)); // Add nearby crows if added to mod
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
-        this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Turtle.class, 10, true, false, Turtle.BABY_ON_LAND_SELECTOR));
+    protected void addBehaviorGoals() {
+        //this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+        //this.goalSelector.addGoal(6, new MoveThroughVillageGoal(this, 1.0D, true, 4, () -> true));
+        this.goalSelector.addGoal(7, new ScarecrowEntity.MoveRandomlyGoal(this, 1.0D));
+        //this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers(ZombifiedPiglin.class)); // Add nearby crows if added to mod
+        //this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        //this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
+        //this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
+        //this.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Turtle.class, 10, true, false, Turtle.BABY_ON_LAND_SELECTOR));
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        long dayTime = getLevel().getDayTime();
-
-        if (dayTime > 13000L && dayTime < 23999L) {
+        if (isNightTime(level)) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.scarecrow.idle_night", true));
         } else {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.scarecrow.idle_day", true));
@@ -72,5 +65,22 @@ public class ScarecrowEntity extends Monster implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return this.factory;
+    }
+
+    public static boolean isNightTime(Level level) {
+        long dayTime = level.getDayTime();
+
+        return dayTime > 13000L && dayTime < 23999L;
+    }
+
+    private static class MoveRandomlyGoal extends WaterAvoidingRandomStrollGoal {
+        public MoveRandomlyGoal(PathfinderMob mob, double probability) {
+            super(mob, probability);
+        }
+
+        @Override
+        public boolean canUse() {
+            return super.canUse() && !isNightTime(mob.level);
+        }
     }
 }
