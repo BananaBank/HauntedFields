c5a6d7f36904db997f834bba4697f1d6ffb671b7
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2022-10-14 19:35:07 -0700

    Template model added for crow with texture and simple animations.

--- src/main/java/team/bananabank/hauntedfields/event/HEventBusEvents.java
+++ src/main/java/team/bananabank/hauntedfields/event/HEventBusEvents.java
@@ -4,6 +4,7 @@ import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import team.bananabank.hauntedfields.HauntedFields;
+import team.bananabank.hauntedfields.entity.CrowEntity;
 import team.bananabank.hauntedfields.entity.ScarecrowEntity;
 import team.bananabank.hauntedfields.registry.HEntityTypes;
 
@@ -13,6 +14,6 @@ public class HEventBusEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(HEntityTypes.SCARECROW.get(), ScarecrowEntity.setAttributes());
-        event.put(HEntityTypes.CROW.get(), ScarecrowEntity.setAttributes());
+        event.put(HEntityTypes.CROW.get(), CrowEntity.setAttributes());
     }
 }
--- src/main/java/team/bananabank/hauntedfields/registry/HItems.java
+++ src/main/java/team/bananabank/hauntedfields/registry/HItems.java
@@ -19,7 +19,7 @@ public class HItems {
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<ForgeSpawnEggItem> CROW_SPAWN_EGG = REGISTRY.register("crow_spawn_egg",
-            () -> new ForgeSpawnEggItem(HEntityTypes.SCARECROW, 0x7a3710, 0xf55a00,
+            () -> new ForgeSpawnEggItem(HEntityTypes.CROW, 0x7a3710, 0xf55a00,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 }
