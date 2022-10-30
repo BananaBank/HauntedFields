ffe73e58c06f3a5df1cbba85fb5320d641d94f94
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2022-10-29 22:24:45 -0700

    Language provider added and updated colors on spawn eggs.

--- src/main/java/team/bananabank/hauntedfields/registry/HItems.java
+++ src/main/java/team/bananabank/hauntedfields/registry/HItems.java
@@ -13,11 +13,11 @@ public class HItems {
     public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, HauntedFields.ID);
 
     public static final RegistryObject<ForgeSpawnEggItem> SCARECROW_SPAWN_EGG = REGISTRY.register("scarecrow_spawn_egg",
-            () -> new ForgeSpawnEggItem(HEntityTypes.SCARECROW, 0x7a3710, 0xf55a00,
+            () -> new ForgeSpawnEggItem(HEntityTypes.SCARECROW, 0x71552d, 0xe38a1d,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<ForgeSpawnEggItem> CROW_SPAWN_EGG = REGISTRY.register("crow_spawn_egg",
-            () -> new ForgeSpawnEggItem(HEntityTypes.CROW, 0x7a3710, 0xf55a00,
+            () -> new ForgeSpawnEggItem(HEntityTypes.CROW, 0x000000, 0x252525,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 }
